c8d0e37d1c4fe823e5c1959aede2f372b17c8449
anjutus <anjutuscano91@gmail.com>
2023-01-20 10:18:35 -0600

    Added Comments,Alignment,restructing of the files

---
+++ src/Components/Dashboard/PieChart.js
@@ -0,0 +1,16 @@
+/*
+*Pie Chart component.
+**@author: Anju Tuscano
+
+*/
+
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export default function PieChart({data}) {
+  return <Pie data={data} />;
+}
