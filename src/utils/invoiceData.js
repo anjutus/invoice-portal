0b08b74a23e640696fc0baf7076dce5c1297ad1c
anjutus <anjutuscano91@gmail.com>
2023-01-24 00:17:23 -0600

    Modified the createInvoice

---
+++ src/utils/invoiceData.js
@@ -0,0 +1,23 @@
+export const invoiceData =
+{
+
+    invoiceID: "",
+    supplierName: "",
+    issueDate: "",
+    dueDate: "",
+    comments: "",
+    invoiceStatus: "",
+    payment: {
+        taxes: 0,
+        discount: 0,
+        totalAmount: 0
+    },
+    invoiceProductDetails: [{
+        productID: 0,
+        productName: "",
+        quantity: 0,
+        price: 0,
+        lineTotal: 0
+    }]
+}
+
