8797d5e2176cd0688a9d9bc08a6e50d9bd47d150
anjutus <anjutuscano91@gmail.com>
2023-03-01 11:35:36 -0600

    Change in Auth UI

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import Auth0ProviderWithHistory from './auth/Auth0ProviderWithHistory';
 import { Auth0Provider } from "@auth0/auth0-react";
 import history from './utils/history';
 import { getConfig } from "./config";
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Auth0ProviderWithHistory>
-//       <App />
-//     </Auth0ProviderWithHistory>
-//   </React.StrictMode>
-// );
-// const domain = "dev-5dathdoezkb70nvo.us.auth0.com";
-// const clientId = "37lzCDNbyonRI9jqm4k9jdAEe5A0SksW";
-// const audience = "https://dev-5dathdoezkb70nvo.us.auth0.com/api/v2/";
-
 
 const onRedirectCallback = (appState) => {
   history.push(
     appState && appState.returnTo ? appState.returnTo : window.location.pathname
   );
- };
- const config = getConfig();
- const providerConfig = {
+};
+const config = getConfig();
+const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   onRedirectCallback,
@@ -35,13 +22,14 @@ const onRedirectCallback = (appState) => {
     ...(config.audience ? { audience: config.audience } : null),
   },
 };
-
-ReactDOM.render(
-  <Auth0Provider  {...providerConfig}
-    >
-    <App/>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+  <Auth0Provider  {...providerConfig}>
+    <App />
   </Auth0Provider>
-  ,document.getElementById("root")
+</React.StrictMode>
+
 );
 
 
